e9bb9d8cb241f2941212b5ec9fb6914ae5fd7f0f
Piotr Karpala <karpik.pl@gmail.com>
2018-03-05 00:43:30 -0500

    ping controller

---
+++ Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pk_dotnet.Controllers
+{
+	[Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        // GET api/ping
+        [HttpGet]
+        public string Get()
+        {
+			return "pong";
+        }
+    }
+}
