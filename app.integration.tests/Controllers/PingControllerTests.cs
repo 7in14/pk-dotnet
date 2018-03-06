be33fc530e023ccd0e05ae6543c8e252fbb59fe0
Piotr Karpala <karpik.pl@gmail.com>
2018-03-05 22:14:19 -0500

    integration tests
    Signed-off-by: Piotr Karpala <karpik.pl@gmail.com>

---
+++ app.integration.tests/Controllers/PingControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pk_dotnet;
+using pk_dotnet.Controllers;
+
+namespace app.integration.tests.Controllers
+{
+	[TestClass]
+	public class PingControllerTests
+	{
+		readonly TestServer _server;
+		readonly HttpClient _client;
+
+		public PingControllerTests()
+		{
+			// Arrange
+			_server = new TestServer(new WebHostBuilder()
+				.UseStartup<Startup>());
+			_client = _server.CreateClient();
+		}
+
+
+		[TestMethod]
+		public async Task Ping_Should_ReturnPong()
+		{
+			// Act
+			var response = await _client.GetAsync("/api/ping");
+			response.EnsureSuccessStatusCode();
+
+			var responseString = await response.Content.ReadAsStringAsync();
+
+			// Assert
+			Assert.AreEqual("pong", responseString);
+		}
+	}
+}
