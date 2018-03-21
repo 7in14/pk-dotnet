0b51dc923d2c9825d0200487667a4685bd9104b3
Piotr Karpala <karpik.pl@gmail.com>
2018-03-20 21:02:39 -0400

    refactored
    Signed-off-by: Piotr Karpala <karpik.pl@gmail.com>

--- src/app/Controllers/AllDataController.cs
+++ src/app/Controllers/AllDataController.cs
@@ -1,10 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Flurl.Http;
 using Microsoft.AspNetCore.Mvc;
 using pkdotnet.DataAccess;
-using Flurl.Http;
-using System.Linq;
-using System;
-using System.Collections.Generic;
 
 namespace pkdotnet
 {
@@ -18,7 +17,6 @@ namespace pkdotnet
 			this.mongoAccess = mongoAccess;
 		}
 
-
 		// GET: /api/allData
 		public async Task<object> Get()
 		{
@@ -26,71 +24,35 @@ namespace pkdotnet
 
 			var toExecute = sources.Select(ds =>
 			{
-				System.Console.WriteLine($"Will be reading data for {ds.Name} from {ds.Url}");
+				Console.WriteLine($"Will be reading data for {ds.Name} from {ds.Url}");
 				return new
 				{
 					name = ds.Name,
-					task = (Task<Object>)ds.Url.GetJsonAsync<object>()
+					task = ds.Url.GetJsonAsync<object>()
 				};
 			}).ToArray();
 
 			var tasks = toExecute.Select(d => d.task).ToArray();
 
-			List<DataResult> data = new List<DataResult>();
-
-			foreach (var t in toExecute)
+			try
 			{
-				object o = null;
-				try
-				{
-					o = await t.task;
-				}
-				catch (Exception ex)
-				{
-
-				}
-
-				data.Add(new DataResult
-				{
-					Name = t.name,
-					Data = o,
-					Error = t.task.IsFaulted ? t.task.Exception.Message : null
-				});
+				Task.WaitAll(tasks);
+				Console.WriteLine("all tasks completed");
+			}
+			catch (AggregateException)
+			{
+				// not everything completed according to plan
+				// ignore and read task.Exception
 			}
 
-			//try
-			//{
-			//	Task.WaitAll(tasks);
-			//	System.Console.WriteLine("all tasks completed");
-			//}
-			//catch (System.AggregateException ex)
-			//{
-			//	// not everything completed according to plan
-			//}
-
-			//var data = toExecute.Select(d => new DataResult
-			//{
-			//	Name = d.name,
-			//	Data = d.task.Result,
-			//	Error = d.task.IsFaulted ? d.task.Exception : null
-			//}).ToList();
+			var data = toExecute.Select(d => new
+			{
+				Name = d.name,
+				Data = d.task.IsCompletedSuccessfully ? d.task.Result : null,
+				Error = d.task.IsFaulted ? d.task.Exception.Message : null
+			}).ToList();
 
 			return data;
-		}
-
-		class DataResult
-		{
-			public string Name;
-			public object Data;
-			public string Error;
-		}
-
-		[Route("test")]
-		public async Task<dynamic> Test()
-		{
-			await "https://jsonplaceholder.typicode.com/users".GetJsonAsync();
-
-			return "hello";
 		}
 	}
 }
